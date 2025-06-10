1d8aeff259ed5610904c652c579f5eda9294dea9
sanjai <sanjai9608@gmail.com>
2025-06-10 11:06:20 +0530

    changed to function to normal arithmetic calculation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-﻿function demo(name,age) {
-    console.log(`Hello my name is ${name}. iam ${age} years old`)
-}
+﻿// function demo(name,age) {
+//     console.log(`Hello my name is ${name}. iam ${age} years old`)
+// }
 
-demo('jhonson',60)
+// demo('jhonson',60)
+
+const  a = 10
+const  b = 20
+
+console.log("print answer : ", a + b)
