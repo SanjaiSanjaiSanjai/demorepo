90913973d49a495ca00640dcf982ab3d5eee28c1
sanjai <sanjai9608@gmail.com>
2025-06-10 11:18:20 +0530

    changed to write print console

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-﻿// function demo(name,age) {
-//     console.log(`Hello my name is ${name}. iam ${age} years old`)
-// }
-
-// demo('jhonson',60)
-
-const  a = 10
-const  b = 20
-
-console.log("print answer : ", a + b)
+﻿console.log("one");
